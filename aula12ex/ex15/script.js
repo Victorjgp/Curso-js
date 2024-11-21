9dfcdb7e86d24e140f0665ed4f17caa7f76f433f
Victorjgp <victor.jgp@hotmail.com>
2024-11-21 15:27:20 -0300

    Exe verificador de idade

--- aula12ex/ex15/script.js
+++ aula12ex/ex15/script.js
@@ -1,1 +1,43 @@
-alert('Olá')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('itxtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //Criança
+            }else if(idade < 21){
+                // Jovem
+            }else if(idade < 50){
+                //Adulto
+            }else{
+                // Idoso
+            }
+        } else if(fsex.checked[1]) {
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //Criança
+            }else if(idade < 21){
+                // Jovem
+            }else if(idade < 50){
+                //Adulto
+            }else{
+                // Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
