b2d3f093b738f84642acd2dca3f204fc3dc5cb12
Victorjgp <victor.jgp@hotmail.com>
2024-11-25 22:58:11 -0300

    ex16

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,1 +1,9 @@
-alert('Olá')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('ERRO faltam dados')
+    }
+}
