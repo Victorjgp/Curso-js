fee7f4e6a720921fd9590fd72a21da2974e1ace0
Victorjgp <victor.jgp@hotmail.com>
2024-11-29 14:12:09 -0300

    Update script.js

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -6,20 +6,29 @@ function contar() {
 
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('ERRO faltam dados')
+        res.innerHTML = 'Impossivel contar'
+       // window.alert('ERRO faltam dados')
+
     } else{
-        res.innerHTML = `Contando: `
+        res.innerHTML = `Contando: <br>`
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if(p <= 0){
+            window.alert('Passo invalido')
+            p = 1
+        }
         if(i < f){
+            //contagem crescente
             for(let c = 1; c <= f; c+= p){
                 res.innerHTML += ` ${c} \u{1F600}	`
-        }
-         
-        for(let c = 1; c <= f; c+= p){
-            res.innerHTML += ` ${c} \u{1F600}	`
+        }       
+
+        }else{
+            //contagem regressiva
+            for(let c = i; c >= f;c -= p){
+                res.innerHTML += `${c}\u{1F600} `
+            }
         }
         res.innerHTML += ` \u{1F601} `
     }
